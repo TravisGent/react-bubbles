8753498ec5d1ed4817d7bd0c913f93a173100d70
Eorium <travisgent312@gmail.com>
2020-07-24 10:48:44 -0600

    add login add data gathering from server add edit and remove functions

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,62 @@
 import React from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import "../styles.scss";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChanges = event => {
+    this.setState({
+      credentials: {
+          ...this.state.credentials,
+          [event.target.name]: event.target.value
+      }
+    });
+  };
+
+  login = event => {
+    event.preventDefault();
+
+    axiosWithAuth()
+      .post("/api/login", this.state.credentials)
+      .then(response => {
+        window.localStorage.setItem("token", response.data.payload)
+        this.props.history.push("/api/colors")
+      })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login To See Data</h1>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChanges} 
+            className="inputField"
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChanges} 
+            className="inputField"
+            placeholder="Password"
+          />
+          <button className="loginButton">Log In</button>
+        </form>
+      </div>
+    )
+  }
+}
 
 export default Login;
